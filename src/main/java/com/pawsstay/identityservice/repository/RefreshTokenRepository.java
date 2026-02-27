228294d00686fa64ef1c3a0bae40fcaf8bcd40a5
wilson <hereisleedo@gmail.com>
2026-02-27 22:08:30 +1300

    feat: refresh token implemented

---
+++ src/main/java/com/pawsstay/identityservice/repository/RefreshTokenRepository.java
@@ -0,0 +1,18 @@
+package com.pawsstay.identityservice.repository;
+
+import com.pawsstay.identityservice.entity.RefreshToken;
+import com.pawsstay.identityservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findRefreshTokenByToken(String token);
+
+    List<RefreshToken> findRefreshTokenByUserOrderByExpiryDateAsc(User user);
+
+
+}
--- src/main/java/com/pawsstay/identityservice/service/AuthServiceImpl.java
+++ src/main/java/com/pawsstay/identityservice/service/AuthServiceImpl.java
@@ -1,10 +1,12 @@
 package com.pawsstay.identityservice.service;
 
 import com.pawsstay.identityservice.dto.*;
+import com.pawsstay.identityservice.entity.RefreshToken;
 import com.pawsstay.identityservice.entity.User;
 import com.pawsstay.identityservice.entity.UserRole;
 import com.pawsstay.identityservice.exception.ConflictException;
 import com.pawsstay.identityservice.exception.UnauthorizedException;
+import com.pawsstay.identityservice.repository.RefreshTokenRepository;
 import com.pawsstay.identityservice.repository.UserRepository;
 import com.pawsstay.identityservice.util.JwtUtils;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
@@ -22,6 +28,7 @@ public class AuthServiceImpl implements AuthService{
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtils jwtUtils;
+    private final RefreshTokenRepository refreshTokenRepository;
     @Value("${app.jwt.expiration}")
     private Long SECRET_EXPIRATION;
     @Value("${app.refresh-token.expiration}")
@@ -45,25 +52,22 @@ public class AuthServiceImpl implements AuthService{
         User user = userRepository.findByEmail(request.getEmail())
                 .orElseThrow(() -> new UnauthorizedException("Invalid email or password"));
         if (passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-            String token = jwtUtils.generateToken(user);
-            String refreshToken = UUID.randomUUID().toString();
-            return AuthResponse.builder()
-                    .accessToken(token)
-                    .refreshToken(refreshToken)
-                    .userId(user.getId())
-                    .userName(user.getUsername())
-                    .email(user.getEmail())
-                    .expiresIn(SECRET_EXPIRATION)
-                    .refreshExpiresIn(REFRESH_TOKEN_EXPIRATION)
-                    .build();
+            cleanRefreshTokens(user);
+            RefreshToken refreshToken = refreshTokenRepository.save(generateRefreshToken(user));
+            return generateAuthResponse(user,refreshToken);
         } else {
             throw new UnauthorizedException("Invalid email or password");
         }
     }
 
     @Override
+    @Transactional
     public AuthResponse refreshToken(String refreshToken) {
-        return null;
+        RefreshToken refreshTokenData = refreshTokenRepository.findRefreshTokenByToken(refreshToken)
+                .orElseThrow(() -> new UnauthorizedException("refresh token not found"));
+        User user = refreshTokenData.getUser();
+        refreshTokenRepository.saveAndFlush(generateRefreshToken(user));
+        return generateAuthResponse(user,refreshTokenData);
     }
 
     @Override
@@ -82,7 +86,7 @@ public class AuthServiceImpl implements AuthService{
                 .orElseThrow(() -> new UnauthorizedException("user not found"));
         user.setEmail(request.getEmail());
         user.setUsername(request.getUsername());
-        user = userRepository.save(user);
+        userRepository.saveAndFlush(user);
 
         return map2RegisterResponse(user);
     }
@@ -104,5 +108,36 @@ public class AuthServiceImpl implements AuthService{
                     .username(request.getUsername())
                     .build();
 
+    }
+
+    private void cleanRefreshTokens(User user){
+        List<RefreshToken> refreshTokenList = refreshTokenRepository.findRefreshTokenByUserOrderByExpiryDateAsc(user);
+        if(refreshTokenList.size() > 10){
+            int numberToRemove = refreshTokenList.size() - 10;
+            List<RefreshToken> toDelete = refreshTokenList.stream()
+                    .limit(numberToRemove)
+                    .toList();
+            refreshTokenRepository.deleteAll(toDelete);
+        }
+    }
+    private RefreshToken generateRefreshToken(User user){
+        String refreshTokenString = UUID.randomUUID().toString();
+        return RefreshToken.builder().token(refreshTokenString)
+                .user(user)
+                .expiryDate(Instant.now().plus(REFRESH_TOKEN_EXPIRATION, ChronoUnit.SECONDS))
+                .build();
+
+    }
+
+    private AuthResponse generateAuthResponse(User user, RefreshToken refreshToken){
+        return AuthResponse.builder()
+                .accessToken(jwtUtils.generateToken(user))
+                .refreshToken(refreshToken.getToken())
+                .userId(user.getId())
+                .userName(user.getUsername())
+                .email(user.getEmail())
+                .expiresIn(SECRET_EXPIRATION)
+                .refreshExpiresIn(REFRESH_TOKEN_EXPIRATION)
+                .build();
     }
 }
